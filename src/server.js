c7b733e3ae7428595e7a5e59f310f96b2ced2692
vianagabriel <vianagabrieldacunha@gmail.com>
2022-10-05 16:16:32 -0300

    commit inicial

---
+++ src/server.js
@@ -0,0 +1,10 @@
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
